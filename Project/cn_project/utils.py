bb2e7bd18fcc3e62afc83e22ed76169a7f6becce
Rafael Bianchi <rafaellfontana@gmail.com>
2019-06-16 00:08:13 +0200

    map & parties

--- Project/cn_project/utils.py
+++ Project/cn_project/utils.py
@@ -1,5 +1,11 @@
+import statistics
+
+import networkx as nx
 import requests
+import numpy as np
 from lxml import etree
+from sklearn.preprocessing import MinMaxScaler
+
 
 def get_content(url):
     response = requests.get(url)
@@ -11,3 +17,24 @@ def get_content(url):
     tree = etree.fromstring(data)
 
     return tree
+
+
+def rescale_weights(graph, min=0, max=1,percentile=90):
+    edges_weights = {(u,v): [d['weight']] for (u, v, d) in graph.edges(data=True)}
+
+    scaler = MinMaxScaler(feature_range=(min,max))
+    weights = scaler.fit_transform(list(edges_weights.values()))
+
+    scaler = MinMaxScaler(feature_range=(0.01,1))
+    colors = scaler.fit_transform(list(edges_weights.values()))
+    edges_colors = {}
+
+    idx = 0
+    for key in edges_weights.keys():
+        graph.edges[(key[0], key[1])]['weight'] = weights[idx][0]
+        edges_weights[(key[0], key[1])] = weights[idx][0]
+        edges_colors[(key[0], key[1])] = colors[idx]
+        
+        idx += 1
+
+    return edges_weights, edges_colors, statistics.median(weights[0]), np.percentile(weights, 90)
--- Project/run.py
+++ Project/run.py
@@ -1,286 +1,140 @@
 #%%
 import pickle
+import random
 
+
+import community
+import matplotlib.pyplot as plt
 import networkx as nx
+from matplotlib.patches import Polygon
+from mpl_toolkits.basemap import Basemap as Basemap
 
-import cn_project.congressman as cm
-import cn_project.parties as parties
-import cn_project.proposition as propostions
-import cn_project.voting as voting
+import cn_project.datafetcher as datafetcher
+import cn_project.utils as utils
 
-refresh_data = False
+# This method starts the fetching process on the 
+# brazilian government website to refresh all the data.
+# The data is pre-stored in the pickle files and then, processed
+# and saved in a pajek network format
+# !!!!! This method might take several hours to complete. !!!!!
+refreshData = False
+if (refreshData):
+    datafetcher.refresh_data()
 
-StateDict={
-            "AC": ( -8.77, -70.55)
-            , "AL": ( -9.71, -35.73)
-            , "AM": ( -3.07, -61.66)
-            , "AP": (  1.41, -51.77)
-            , "BA": (-12.96, -38.51)
-            , "CE": ( -3.71, -38.54)
-            , "DF": (-15.83, -47.86)
-            , "ES": (-19.19, -40.34)
-            , "GO": (-16.64, -49.31)
-            , "MA": ( -2.55, -44.30)
-            , "MT": (-12.64, -55.42)
-            , "MS": (-20.51, -54.54)
-            , "MG": (-18.10, -44.38)
-            , "PA": ( -5.53, -52.29)
-            , "PB": ( -7.06, -35.55)
-            , "PR": (-24.89, -51.55)
-            , "PE": ( -8.28, -35.07)
-            , "PI": ( -8.28, -43.68)
-            , "RJ": (-22.84, -43.15)
-            , "RN": ( -5.22, -36.52)
-            , "RO": (-11.22, -62.80)
-            , "RS": (-30.01, -51.22)
-            , "RR": (  1.89, -61.22)
-            , "SC": (-27.33, -49.44)
-            , "SE": (-10.90, -37.07)
-            , "SP": (-23.55, -46.64)
-            , "TO": (-10.25, -48.25)
-}
+# parties_pickle = open("cn_project/data/parties.pickle","rb")
+# ps = pickle.load(parties_pickle)
 
-#%%
-if(refresh_data):
-    #Refreshing parties
-    ps = parties.Party.get_all_parties()
-    ps_dic = {}
-    for p in ps:
-        ps_dic[p.id] = p
+# props_pickle = open("cn_project/data/propositions.pickle","rb")
+# props = pickle.load(props_pickle)
 
-    parties_pickle = open('cn_project/data/parties.pickle', 'wb')
-    pickle.dump(ps_dic, parties_pickle)
-    parties_pickle.close()
+# vote_pickle = open("cn_project/data/votes.pickle","rb")
+# vs = pickle.load(vote_pickle)
 
-    #Refreshing propositions
-    props = propostions.Proposition.get_all_propositions(1991, 2020)
-    props_pickle = open('cn_project/data/propositions.pickle', 'wb')
-    pickle.dump(props, props_pickle)
-    props_pickle.close()
-
-    #Refreshing votings
-    vote_pickle = open('cn_project/data/votes.pickle', 'wb')
-    congressman_to_check = {}
-    votes = {}
-    try:
-        count = 1
-        for proposition in props.keys():
-            print(f'Getting votes for proposition {proposition} ({count}/{len(props.keys())})')
-            v = voting.Voting.get_votes(props[proposition].number, props[proposition].type, props[proposition].year)
-            for check in v:
-                for c in check.congressmen_votes.keys():
-                    if check.congressmen_votes[c].id_congressman not in congressman_to_check.keys():
-                        congressman_to_check[c] = check.congressmen_votes[c]
-            votes[proposition] = v
-
-            count += 1
-    finally:
-        pickle.dump(votes, vote_pickle)
-        vote_pickle.close()
-
-    #Refreshing congressman details
-    congressmen = {}
-    count = 1
-    for c_id in congressman_to_check.keys():
-        print(f'Getting info for congressman id {c_id} ({count}/{len(congressman_to_check)})')
-        congressman = cm.Congressman(c_id, c_id.isdigit())
-        
-        if (congressman.full_name == None or congressman.full_name == ''):
-            print('Using info from voting')
-            congressman.cod_state_repr = congressman_to_check[c_id].state
-            congressman.full_name = congressman_to_check[c_id].name
-            congressman.display_name = congressman_to_check[c_id].name
-            congressman.id_current_party = congressman_to_check[c_id].party
-
-        congressmen[c_id] = congressman
-        count += 1
-
-    congressmen_pickle = open('cn_project/data/congressmen.pickle', 'wb')
-    pickle.dump(congressmen, congressmen_pickle)
-    congressmen_pickle.close()
-
-    g_congressmen = nx.Graph()
-    g_parties = nx.Graph()
-    g_states = nx.Graph()
-
-    prop_count = 1
-    for prop in votes:
-        print(f'Starting proposition {prop} ({prop_count}/{len(vs)})')
-        for v in vs[prop]:
-            cmv_votes = {}
-            
-            for cmv in v.congressmen_votes:
-                if cmv not in cms:
-                    continue
-
-                vote = v.congressmen_votes[cmv].vote.lower()
-
-                if vote not in cmv_votes:
-                    cmv_votes[vote] = []
-                
-                cmv_votes[vote].append(v.congressmen_votes[cmv])
-
-            for cmv_key in cmv_votes.keys():
-                cmsv = cmv_votes[cmv_key]
-                for i in range(0, len(cmsv)):
-                    congressman1 = cms[cmsv[i].id_congressman]
-                    party1 = cmsv[i].party.upper()
-                    state1 = cmsv[i].state.upper()
-                    
-                    pos1 = StateDict[congressman1.cod_state_repr.upper()]
-                    
-                    if (not g_congressmen.has_node(congressman1.display_name)):
-                        g_congressmen.add_node(congressman1.display_name, x = pos1[0], y = pos1[1])
-                    
-                    if (not g_parties.has_node(party1)):
-                        g_parties.add_node(party1)
-
-                    if (not g_states.has_node(state1)):
-                        g_states.add_node(state1, x = StateDict[state1][0], y = StateDict[state1][1])
-
-                    for j in range(i+1, len(cmsv)):
-                        congressman2 = cms[cmsv[j].id_congressman]
-                        party2 = cmsv[j].party.upper()
-                        state2 = cmsv[j].state.upper()
-
-                        #just in case ...
-                        if(congressman1 == congressman2):
-                            continue
-
-                        #Congressmen vote
-                        if (not g_congressmen.has_node(congressman2.display_name)):
-                            pos2 = StateDict[congressman1.cod_state_repr.upper()]
-                            g_congressmen.add_node(congressman2.display_name, x = pos2[0], y = pos2[1])
-                        
-                        if g_congressmen.has_edge(congressman1.display_name, congressman2.display_name):
-                            g_congressmen[congressman1.display_name][congressman2.display_name]['weight'] += 1
-                        else:
-                            g_congressmen.add_edge(congressman1.display_name, congressman2.display_name, weight=1)
-
-                        #Party vote
-                        if (party1 != party2):
-                            if (not g_parties.has_node(party2)):
-                                g_parties.add_node(party2)
-                            
-                            if g_parties.has_edge(party1, party2):
-                                g_parties[party1][party2]['weight'] += 1
-                            else:
-                                g_parties.add_edge(party1, party2, weight = 1)
-                        
-                        #State vote
-                        if (state1 != state2):
-                            if (not g_states.has_node(state2)):
-                                g_states.add_node(state2, x = StateDict[state2][0], y = StateDict[state2][1])
-                            
-                            if g_states.has_edge(state1, state2):
-                                g_states[state1][state2]['weight'] += 1
-                            else:
-                                g_states.add_edge(state1, state2, weight=1)
-            session_count += 1
-        prop_count += 1
-
-    nx.write_pajek(g_congressmen, 'cn_project/data/voting_relation_congressmen.net')
-    nx.write_pajek(g_parties, 'cn_project/data/g_parties.net')
-    nx.write_pajek(g_states, 'cn_project/data/g_states.net')
-    print('finished')
-else:    
-    parties_pickle = open("cn_project/data/parties.pickle","rb")
-    ps = pickle.load(parties_pickle)
-
-    props_pickle = open("cn_project/data/propositions.pickle","rb")
-    props = pickle.load(props_pickle)
-
-    vote_pickle = open("cn_project/data/votes.pickle","rb")
-    vs = pickle.load(vote_pickle)
-
-    congressman_pickle = open("cn_project/data/congressmen.pickle","rb")
-    cms = pickle.load(congressman_pickle)
-
-    g_congressmen = nx.Graph()
-    g_parties = nx.Graph()
-    g_states = nx.Graph()
+# congressman_pickle = open("cn_project/data/congressmen.pickle","rb")
+# cms = pickle.load(congressman_pickle)
 
 
-    prop_count = 1
-    for prop in vs:
-        print(f'Starting proposition {prop} ({prop_count}/{len(vs)})')
-        for v in vs[prop]:
-            cmv_votes = {}
-            
-            for cmv in v.congressmen_votes:
-                if cmv not in cms:
-                    continue
+###########################################################################
+# Start - Parties - Community detection and ploting 
+###########################################################################
+g_parties = nx.Graph(nx.read_pajek( 'cn_project/data/g_parties.net'))
 
-                vote = v.congressmen_votes[cmv].vote.lower()
+partition = community.best_partition(g_parties)
+size = float(len(set(partition.values())))
+pos = nx.circular_layout(g_parties, scale=.1)
 
-                if vote not in cmv_votes:
-                    cmv_votes[vote] = []
-                
-                cmv_votes[vote].append(v.congressmen_votes[cmv])
+p_values = list(pos.values())
+random.shuffle(p_values)
 
-            for cmv_key in cmv_votes.keys():
-                cmsv = cmv_votes[cmv_key]
-                for i in range(0, len(cmsv)):
-                    congressman1 = cms[cmsv[i].id_congressman]
-                    party1 = cmsv[i].party.upper()
-                    state1 = cmsv[i].state.upper()
-                    
-                    pos1 = StateDict[congressman1.cod_state_repr.upper()]
-                    
-                    if (not g_congressmen.has_node(congressman1.display_name)):
-                        g_congressmen.add_node(congressman1.display_name, x = pos1[0], y = pos1[1])
-                    
-                    if (not g_parties.has_node(party1)):
-                        g_parties.add_node(party1)
+i = 0
+for p in pos.keys():
+    pos[p] = p_values[i]
+    i += 1
 
-                    if (not g_states.has_node(state1)):
-                        g_states.add_node(state1, x = StateDict[state1][0], y = StateDict[state1][1])
+nodes_index = {}
 
-                    for j in range(i+1, len(cmsv)):
-                        congressman2 = cms[cmsv[j].id_congressman]
-                        party2 = cmsv[j].party.upper()
-                        state2 = cmsv[j].state.upper()
+for node_name, node in g_parties.node.items(): 
+    nodes_index[node_name] = int(node['id'])-1
 
-                        #just in case ...
-                        if(congressman1 == congressman2):
-                            continue
+dict_communities = community.best_partition(g_parties)
 
-                        #Congressmen vote
-                        if (not g_congressmen.has_node(congressman2.display_name)):
-                            pos2 = StateDict[congressman1.cod_state_repr.upper()]
-                            g_congressmen.add_node(congressman2.display_name, x = pos2[0], y = pos2[1])
-                        
-                        if g_congressmen.has_edge(congressman1.display_name, congressman2.display_name):
-                            g_congressmen[congressman1.display_name][congressman2.display_name]['weight'] += 1
-                        else:
-                            g_congressmen.add_edge(congressman1.display_name, congressman2.display_name, weight=1)
+com_linear = [None] * len(g_parties)
 
-                        #Party vote
-                        if (party1 != party2):
-                            if (not g_parties.has_node(party2)):
-                                g_parties.add_node(party2)
-                            
-                            if g_parties.has_edge(party1, party2):
-                                g_parties[party1][party2]['weight'] += 1
-                            else:
-                                g_parties.add_edge(party1, party2, weight = 1)
-                        
-                        #State vote
-                        if (state1 != state2):
-                            if (not g_states.has_node(state2)):
-                                g_states.add_node(state2, x = StateDict[state2][0], y = StateDict[state2][1])
-                            
-                            if g_states.has_edge(state1, state2):
-                                g_states[state1][state2]['weight'] += 1
-                            else:
-                                g_states.add_edge(state1, state2, weight=1)
-                        
-        prop_count += 1
+for n_id in nodes_index:
+    com_linear[nodes_index[n_id]] = dict_communities[n_id]
 
+nx.draw_networkx_nodes(g_parties, pos, cmap='jet', node_color=com_linear, node_size=100)
+
+edges_weights, _, _, _ = utils.rescale_weights(g_parties, 0.1, 10)
+
+nx.draw_networkx_edges(g_parties, pos, width=list(edges_weights.values()))
+
+nx.draw_networkx_labels(g_parties, pos, font_size=14, font_color='#008080', font_family='sans-serif')
+
+plt.axis('off')
+plt.show()
+###########################################################################
+# End - Parties - Community detection and ploting 
+###########################################################################
+
+
+###########################################################################
+# Start - Parties - Community detection and ploting 
+###########################################################################
+g_states = nx.Graph(nx.read_pajek('cn_project/data/g_states.net'))
+x = nx.get_node_attributes(g_states, 'x')
+y = nx.get_node_attributes(g_states, 'y')
+
+fig, ax = plt.subplots()
+m = Basemap(projection='merc', llcrnrlat=-35, urcrnrlat=7,
+            llcrnrlon=-77, urcrnrlon=-32, resolution='i')
+m.ax = ax
+m.drawstates()
+
+shp = m.readshapefile('cn_project\mapdata\gadm36_BRA_0', 'states', drawbounds=True)
+for nshape, seg in enumerate(m.states):
+    poly = Polygon(seg, facecolor='#00db00', edgecolor='k',zorder=0)
+    ax.add_patch(poly)
+
+# position in decimal lat/lon
+lats= list(x.values())
+lons= list(y.values())
+# convert lat and lon to map projection
+mx,my=m(lons,lats)
+
+# The NetworkX part
+# put map projection coordinates in pos dictionary
+count=0
+m_based_locs = {}
+for key in x:
+    m_based_locs[key] = (mx[count], my[count])
+    count += 1
+
+# draw
+nx.draw_networkx_nodes(g_states,m_based_locs,node_size=100,node_color='k')
+
+edges_weights, edges_colors, _, _ = utils.rescale_weights(g_states, 0.1, 3)
+
+for edge in edges_weights:
+    nx.draw_networkx_edges(g_states, m_based_locs, edgelist=[edge],
+                       width=edges_weights[edge], edge_color=str(edges_colors[edge][0]))
+
+nx.draw_networkx_labels(g_states, m_based_locs, font_size=14, font_color='white', font_family='sans-serif')
+
+plt.show()
+
+
+###########################################################################
+# End - Parties - Community detection and ploting 
+###########################################################################
+
+
+# nx.draw(g_parties, pos=nx.spring_layout(g_parties))
+# plt.draw()  # pyplot draw(
+# plt.show()
+
+# g_congressmen = nx.Graph(nx.read_pajek( 'cn_project/data/voting_relation_congressmen.net'))
+
+# g_states = nx.Graph(nx.read_pajek( 'cn_project/data/g_states.net'))
 
 #%%
-nx.write_pajek(g_congressmen, 'cn_project/data/voting_relation_congressmen.net')
-nx.write_pajek(g_parties, 'cn_project/data/g_parties.net')
-nx.write_pajek(g_states, 'cn_project/data/g_states.net')
-
-print('finished')
